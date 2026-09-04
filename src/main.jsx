23be4dbd279ec4a3433a4cff934d4d6577a483d0
Ahmad Naeem <an1748452@gmail.com>
2026-09-04 21:17:51 +0500

    Add Zanderio chatbot

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,23 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom'
-import App from './App'
-import './index.css'
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+import App from "./App";
+import "./index.css";
+
+const zanderioScript = document.createElement("script");
+zanderioScript.src = "https://cdn.zanderio.ai/widget/loader.js";
+zanderioScript.setAttribute(
+  "data-id",
+  "wdg_tsbXk35HZzMLdomF3FUXnhub"
+);
+zanderioScript.defer = true;
+
+document.body.appendChild(zanderioScript);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
   <BrowserRouter>
     <App />
   </BrowserRouter>
-)
+);
+
